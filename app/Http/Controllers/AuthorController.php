8765282a9e6aee836b8045bb022b221eb93c1da4
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-06 12:51:07 +0300

    feat(controllers): make methods for AuthorController with service

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Author;
+use App\Services\AuthorService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -12,59 +12,51 @@ class AuthorController extends Controller
     use ApiResponser;
 
     /**
+     * The service to consume the authors microservice
+     * @var AuthorService
+     */
+    public $authorService;
+
+    /**
      * Create a new controller instance.
      *
-     * @return void
+     * @param AuthorService $authorService
      */
-    public function __construct()
+    public function __construct(AuthorService $authorService)
     {
-        //
+        $this->$authorService = $authorService;
     }
 
     /**
      * Return the list of authors
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return Response
      */
     public function index()
     {
-        $authors = Author::all();
-        return $this->successResponse($authors);
+        return $this->successResponse($this->authorService->obtainAuthors());
     }
 
     /**
      * Create one new author
      *
      * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     * @throws \Illuminate\Validation\ValidationException
+     * @return Response
      */
     public function store(Request $request)
     {
-        $rules = [
-            'name' => 'required|max:255',
-            'gender' => 'required|max:255|in:male,female',
-            'country' => 'required|max:255',
-        ];
-
-        $this->validate($request, $rules);
-
-        $author = Author::create($request->all());
-
-        return $this->successResponse($author, Response::HTTP_CREATED);
+        return $this->successResponse($this->authorService->createAuthor($request->all(), Response::HTTP_CREATED));
     }
 
     /**
      * Obtains and show one author
      *
      * @param $author
-     * @return \Illuminate\Http\JsonResponse
+     * @return Response
      */
     public function show($author)
     {
-        $author = Author::findOrFail($author);
-
-        return $this->successResponse($author);
+        return $this->successResponse($this->authorService->obtainAuthor($author));
     }
 
     /**
@@ -72,45 +64,21 @@ class AuthorController extends Controller
      *
      * @param Request $request
      * @param $author
-     * @return \Illuminate\Http\JsonResponse
-     * @throws \Illuminate\Validation\ValidationException
+     * @return Response
      */
     public function update(Request $request, $author)
     {
-        $rules = [
-            'name' => 'max:255',
-            'gender' => 'max:255|in:male,female',
-            'country' => 'max:255',
-        ];
-
-        $this->validate($request, $rules);
-
-        $author = Author::findOrFail($author);
-
-        $author->fill($request->all());
-
-        if ($author->isClean()) {
-            return $this->errorResponse('At least one value must change.', Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
-
-        $author->save();
-
-        return $this->successResponse($author);
-
+        return $this->successResponse($this->authorService->editAuthor($request->all(), $author));
     }
 
     /**
      * Remove an existing author
      *
      * @param $author
-     * @return \Illuminate\Http\JsonResponse
+     * @return Response
      */
     public function destroy($author)
     {
-        $author = Author::findOrFail($author);
-
-        $author->delete();
-
-        return $this->successResponse($author);
+        return $this->successResponse($this->authorService->deleteAuthor($author));
     }
 }
